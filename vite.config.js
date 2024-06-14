2a6ee853b30abf44c00231f84d2e083d59c8b574
KhaiZulfa18 <khaizulfa18@gmail.com>
2024-06-14 23:26:39 +0700

    test vite

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
         hmr: {
             host: 'localhost',
+            port: 5173
+        },
+        watch: {
+            usePolling: true,
         },
     },
     plugins: [
