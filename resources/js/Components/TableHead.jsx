85536fc70de9533f5122dc63d4e3213ab36815b3
KhaiZulfa18 <khaizulfa18@gmail.com>
2024-06-19 23:56:08 +0700

    test

---
+++ resources/js/Components/TableHead.jsx
@@ -0,0 +1,7 @@
+export default function TableHead({}) {
+    return (
+        <div>
+            Table Head
+        </div>
+    );
+}
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,12 +1,25 @@
+import DangerButton2 from '@/Components/Button';
+import DangerButton from '@/Components/DangerButton';
+import TableHead from '@/Components/TableHead';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import DashbordLayout from '@/Layouts/Dashboard';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
     return (
-        <DashbordLayout>
-            <h1 className="text-4xl font-bold">Dashboard</h1>
-            <p className="mt-4">Welcome to the dashboard!</p>
-        </DashbordLayout>
+        <AuthenticatedLayout
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+        >
+
+        <Head title="Dashoard"></Head>
+
+        <TableHead />
+
+        <DangerButton2>
+            Button
+        </DangerButton2>
+            
+        </AuthenticatedLayout>
     );
 }
