63f3d2ad924b89606030b64f7e18b0039fdb41dd
KhaiZulfa18 <khaizulfa18@gmail.com>
2024-06-26 23:20:55 +0700

    add darkmode

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import React, { useEffect, useState } from 'react'
+import { useTheme } from '@/Context/ThemeSwitcherContext';
 
 export default function AppLayout({children}) {
 
-    const darkMode = false;
+    // destruct darkMode and themeSwitcher from context
+    const {darkMode, themeSwitcher } = useTheme;
 
+    // define state sidebarOpen
     const [sidebarOpen, setSidebarOpen] = useState(
         localStorage.getItem('sidebarOpen') === 'true'
     );
@@ -20,14 +23,13 @@ export default function AppLayout({children}) {
     
     return (
         <div className='min-h-screen flex overflow-y-auto'>
-            <Sidebar sidebarOpen={sidebarOpen}/>
+            <Sidebar sidebarOpen={sidebarOpen} />
             <div className='flex-1 flex-col overflow-y-auto h-screen'>
-                {/* <Navbar toggleSidebar={toggleSidebar} themeSwitcher={themeSwitcher} darkMode={darkMode} /> */}
-                <Navbar toggleSidebar={toggleSidebar}  darkMode={darkMode}/>
-                {/* <div className='w-full py-8 px-4 md:px-6 min-h-screen overflow-y-auto mb-14 md:mb-0 text-white'>
-                    <Toaster position='top-right'/>
+                <Navbar toggleSidebar={toggleSidebar} themeSwitcher={themeSwitcher} darkMode={darkMode}/>
+                <div className='w-full py-8 px-4 md:px-6 min-h-screen overflow-y-auto mb-14 md:mb-0 text-white'>
+                    {/* <Toaster position='top-right'/> */}
                     {children}
-                </div> */}
+                </div>
             </div>
         </div>
     );
