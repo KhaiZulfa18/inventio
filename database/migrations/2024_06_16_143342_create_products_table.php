5f1477ade4e4abf8dcfa4328644390ee66f9597b
KhaiZulfa18 <khaizulfa18@gmail.com>
2024-07-07 23:05:25 +0700

    change product database

--- database/migrations/2024_06_16_143342_create_products_table.php
+++ database/migrations/2024_06_16_143342_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
+            $table->string('unit')->nullable();
+            $table->double('weight',8,2)->nullable();
+            $table->string('code')->nullable();
+            $table->enum('status', ['0','1','2'])->nullable()->default('1')->comment('0: non-active, 1: active, 2: discontinued');
             $table->foreignId('created_by')->nullable()->constrained('users');
             $table->foreignId('updated_by')->nullable()->constrained('users');
             $table->foreignId('deleted_by')->nullable()->constrained('users');
