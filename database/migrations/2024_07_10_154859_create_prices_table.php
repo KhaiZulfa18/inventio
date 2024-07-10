8022c9d9a919d216058ff94d011b1b57651a813b
KhaiZulfa18 <khaizulfa18@gmail.com>
2024-07-10 22:52:26 +0700

    add price

---
+++ database/migrations/2024_07_10_154859_create_prices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id')->constrained('products');
+            $table->decimal('price',10,2);
+            $table->dateTime('start_date');
+            $table->dateTime('end_data')->nullable();
+            $table->enum('status', ['0','1'])->default('1')->comment('0: nonactive, 1: active');
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
+            $table->foreignId('deleted_by')->nullable()->constrained('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prices');
+    }
+};
